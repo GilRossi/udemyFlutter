c64b50bf2df38be7afd56fc441917a8d9eb6d22b
Gil <gil.rossi.aguiar@live.com>
2024-10-02 13:32:42 -0300

    commit dev 1

--- projeto_perguntas/lib/questionario.dart
+++ projeto_perguntas/lib/questionario.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import './questao.dart';
 import './resposta.dart';
